5b1367d287b9d067e4e15a912e29c2d174663fec
Andrew Park <atpark333@gmail.com>
2022-07-11 14:34:06 +0900

    Change promise results in await

--- await.js
+++ await.js
@@ -1,7 +1,7 @@
 const log = console.log;
 
 // Try changing this to `false` to see how async errors are handled
-const coffeeGrinderWorks = true;
+const coffeeGrinderWorks = false;
 
 function boilWater() {
   return new Promise((resolve, reject) => {
@@ -9,9 +9,9 @@ function boilWater() {
     log("Step 1: Boil water");
     setTimeout(() => {
       console.log("Done boiling");
-      resolve("HOT WATER"); // Promise is RESOLVED
+      resolve(true); // Promise is RESOLVED
     }, 2000); // resolve after 2 seconds
-  })
+  });
 }
 
 function grindCoffee() {
@@ -21,7 +21,7 @@ function grindCoffee() {
     if (coffeeGrinderWorks) {
       setTimeout(() => {
         console.log("Ground coffee");
-        resolve("COFFEE GRINDS"); // Promise is RESOLVED
+        resolve(true); // Promise is RESOLVED
       }, 2000); // resolve after 2 seconds
     } else {
       reject("Coffee grinder is broken!"); // Promise is REJECTED
@@ -35,7 +35,7 @@ function brewCoffee(groundCoffee) {
     // Promise is PENDING
     setTimeout(() => {
       console.log(`Brewed coffee with ${groundCoffee}`);
-      resolve("COFFEE"); // Promise is RESOLVED
+      resolve(true); // Promise is RESOLVED
     }, 4000); // resolve after 2 seconds
   });
 }
@@ -49,18 +49,18 @@ function drinkCoffee(coffee) {
  * the order of async function execution
  * using .then
  */
-async function makeCoffeeAwait() {
+async function start() {
   try {
     const boilResult = await boilWater();
-    const grindResult = await grindCoffee(boilResult);
-    const brewResult = await brewCoffee(grindResult);
-    drinkCoffee(brewResult);
+    console.log(boilResult);
+    const grindResult = await grindCoffee();
+    console.log(grindResult);
+    const brewResult = await brewCoffee();
+    console.log(brewResult);
+    drinkCoffee();
   } catch(error) {
     console.log(error);
   }
 }
 
-
-makeCoffeeAwait();
-
-console.log("Sync Code is done!");
+start();
