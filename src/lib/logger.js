84c28fd828c4b707e6eb445431d2ec9521f46203
Andrew Park <atpark333@gmail.com>
2022-07-12 22:45:44 +0900

    Refactor the logger module

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,37 +1,18 @@
 class Logger {
-  constructor() {
-    this.statuses = {
-      OK: 0,
-      ERROR: 1,
-      WAIT: 2
-    };
+  success(message) {
+    this.log(message, '✅');
   }
 
-  log(message, status) {
-    const icon = this.getIcon(status);
+  error(message) {
+    this.log(message, '❌');
+  }
+
+  pending(message) {
+    this.log(message, '⏳');
+  }
+
+  log(message, icon) {
     console.log(`${icon} ${message}`);
-  }
-
-  getIcon(status) {
-    let icon;
-    const { OK, ERROR, WAIT } = this.statuses;
-
-    switch (status) {
-      case OK:
-        icon = '✅';
-        break;
-      case ERROR:
-        icon = '❌';
-        break;
-      case WAIT:
-        icon = '⏳';
-        break;
-      default:
-        icon = '';
-        break;
-    }
-
-    return icon;
   }
 }
 
